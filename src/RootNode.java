ea8857cb655d09e91c5f6fcf41e46e2ae9fcc7c6
Bongani X <velanibzuma@gmail.com>
2025-09-02 13:27:00 +0200

    -defined constructor of root node

--- src/RootNode.java
+++ src/RootNode.java
@@ -1,3 +1,13 @@
 public class RootNode<T extends Comparable<T>> {
-    
+    T data;
+    //left and right extension of node
+    RootNode<T> left;
+    RootNode<T> right;
+
+    public RootNode(T data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
 }
