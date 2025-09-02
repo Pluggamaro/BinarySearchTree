c3745838f649559523c07f4ff1bb12effc77686b
Bongani X <velanibzuma@gmail.com>
2025-09-02 17:03:34 +0200

    -node addition success and orderly visualization

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         //Instantiating BST with type Integer
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        BinarySearchTree<Integer> binarySearchObject = new BinarySearchTree<>();
 
-        
+        System.out.println("========Node INSERTION========");
+        binarySearchObject.insert(50);
+        binarySearchObject.insert(30);
+        binarySearchObject.insert(70);
+        binarySearchObject.insert(20);
+        binarySearchObject.insert(40);
+        binarySearchObject.insert(60);
+        binarySearchObject.insert(80);
+
+        binarySearchObject.inOrder();
     }
 }
