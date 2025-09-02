ca19da1f4976d191016304b3e8764d606fdf368c
Bongani X <velanibzuma@gmail.com>
2025-09-02 13:52:01 +0200

    -using recursion to traverse tree nodes -logic assumes main root is 0 hence any negative is placed on the left and right otherwise

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,30 @@
-public class BinarySearchTree {
+public class BinarySearchTree<T extends Comparable<T>> {
+    //defining root
+    private RootNode<T> root;
     
+    public BinarySearchTree(){
+        this.root = null;
+    }
+
+    //tree insertion
+    public void insert(T data){
+        root = insertRecursive(root, data);
+    }
+
+    //helper method for insertion
+    private RootNode<T> insertRecursive(RootNode<T> currentNode, T newData){
+        //if currentNode is null then newData is 
+        if(currentNode == null){
+            return new RootNode<>(newData);
+        }
+
+        //otherwise compare new data to root node for traversal route
+        int compare = newData.compareTo(currentNode.data);
+
+        //negatives on left side of tree and positives on right
+        if(compare < 0){
+            currentNode.left = insertRecursive(currentNode.left, newData);
+        }
+        return currentNode;
+    }
 }
